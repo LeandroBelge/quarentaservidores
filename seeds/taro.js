f91e30b18388aa0ace9500b99b6eb320c3e2b4f4
unknown <leandrobelge@gmail.com>
2021-05-20 10:51:51 -0300

    Teste heroku

--- seeds/taro.js
+++ seeds/taro.js
@@ -3,11 +3,5 @@ import seeder from 'knex-csv-seeder';
 exports.seed = seeder({
   table: 'users',
   file: '/path/to/users.csv',
-  // recordsPerQuery: 100,
-  // encoding: 'utf8' default encoding
-  // parser: {
-  //   delimiter: ',',
-  //   quote: '"',
-  //   escape: '\\'
-  // }
+  
 });
